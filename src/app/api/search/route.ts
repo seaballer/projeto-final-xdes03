57a428b61edc70d3f5beb9ddee90216958fede0c
Henrique <henriquebatistamg@gmail.com>
2025-07-04 14:46:11 -0300

    Algumas alterações no create, ainda nao está totalmente pronto

---
+++ src/app/api/search/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+import axios from 'axios'
+
+export async function GET(request: Request) {
+  const apiKey = process.env.RAWG_API_KEY;
+  const { searchParams } = new URL(request.url);
+  const query = searchParams.get('query');
+
+  const url = `https://api.rawg.io/api/games?key=${apiKey}&search=${query}`;
+
+  const {data} = await axios.get(url);
+  
+  return NextResponse.json(data);
+}
